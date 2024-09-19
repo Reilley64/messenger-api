08083faf615da5d2e49768362559ed743bbc9bd5
Reilley <reilleygray@gmail.com>
2024-09-19 18:51:38 +1000

    Refactor error handling for user retrieval
    Simplify the error handling logic by chaining method calls and changing the error message to "User not found" when the user is not found in the repository. This improves readability and aligns the error messaging with our standard conventions.

--- src/controllers/auth_controller.rs
+++ src/controllers/auth_controller.rs
@@ -18,10 +18,10 @@ pub async fn get_auth_user(_: BearerAuth, data: Data<AppState>) -> Result<Json<U
                                 Problem::InternalServerError("failed to retrieve sub from  app data".to_string())
                         })?;
 
-        let auth_user = data.user_repository.find_by_sub(sub)?.ok_or_else(|| {
-                error!("failed to find auth user with sub");
-                Problem::InternalServerError("failed to find auth user with sub".to_string())
-        })?;
+        let auth_user = data
+                .user_repository
+                .find_by_sub(sub)?
+                .ok_or(Problem::NotFound("User not found".to_string()))?;
 
         Ok(Json(UserResponseDto::from(auth_user)))
 }
